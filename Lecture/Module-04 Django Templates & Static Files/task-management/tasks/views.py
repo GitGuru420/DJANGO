3dd93360e986eb54831126a834b1d57a3c38cf98
GitGuru420 <raisulislam1892@gmail.com>
2025-07-03 04:02:25 +0600

    Django Templates & Static Files

---
+++ Lecture/Module-04 Django Templates & Static Files/task-management/tasks/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def manager_dashboard(request):
+    return render(request, "dashboard/manager-dashboard.html")
+
+def user_dashboard(request):
+    return render(request, "dashboard/user-dashboard.html")
+
+def test(request):
+    names = ["Mahmud", "Ahmed", "John", "Sunny Leone"]
+    count = 0
+    for name in names:
+        count += 1
+    context = {
+        "names": ["Mahmud", "Ahmed", "John", "Sunny Leone"],
+        "age" : 23,
+        "count": count
+    }
+    return render(request, "test.html", context)
