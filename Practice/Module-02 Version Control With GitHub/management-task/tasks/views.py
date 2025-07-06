f1cd77d88a76c6d135ba6343804ebfb86f55fb64
GitGuru420 <raisulislam1892@gmail.com>
2025-07-06 10:32:56 +0600

    Version Control With GitHub

---
+++ Practice/Module-02 Version Control With GitHub/management-task/tasks/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Home View
+def home_view(request):
+    return HttpResponse("<h1>Welcome to the Task Management System</h1>")
+
+# Contact View
+def contact_view(request):
+    return HttpResponse("<h1>Welcome to the Task Management System Contact Page</h1>")
+
+# About View
+def about_view(request):
+    return HttpResponse("<h1>About Us: This is the Task Management System where you can manage your tasks effectively</h1>")
+
+# Services View
+def services_view(request):
+    return HttpResponse("<h1>Services: This displays our system services</h1>")
