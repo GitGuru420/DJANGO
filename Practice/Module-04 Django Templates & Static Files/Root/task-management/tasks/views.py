f130aab957e5c00056b97844ec80e6be939e0599
GitGuru420 <raisulislam1892@gmail.com>
2025-08-02 09:12:25 +0600

    Django Templates & Static Files - Root

---
+++ Practice/Module-04 Django Templates & Static Files/Root/task-management/tasks/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Home View
+def home_view(request):
+    return render(request, "home.html")
+
+# Contact View
+def contact_view(request):
+    return HttpResponse("<h1>This is the contact page. Reach us at contact@taskmanagement.com</h1>")
+
+# About View
+def about_view(request):
+    return HttpResponse("<h1>About US: This is the 'Task Management System' where you can manage your tasks effectively</h1>")
+
+# Show View
+def show_view(request):
+    return HttpResponse("<h1>Show Task: This display task details and related information</h1>")
