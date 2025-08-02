b22f011b0113bc7ab595233b3b1be80194313fec
GitGuru420 <raisulislam1892@gmail.com>
2025-08-02 10:09:33 +0600

    Django Templates & Static Files - App

---
+++ Practice/Module-04 Django Templates & Static Files/App/task-management/tasks/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def manager_dashboard(request):
+    return render(request, "dashboard/manager-dashboard.html")
+
+def user_dashboard(request):
+    return render(request, "dashboard/user-dashboard.html")
+
+def test(request):
+    return render(request, 'test.html')
+
+# context
+def home(request):
+    context = {
+        'name' : 'Raisul Islam',
+        'age' : 24,
+        'hobbies' : ['Cricket', 'Music', 'Cycling']
+    }
+    return render(request, 'home.html', context)
